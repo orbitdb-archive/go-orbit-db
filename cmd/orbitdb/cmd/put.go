c304be921e7d96f8937606752ba571b9a666b152
haydenyoung <hayden@knowledgearc.com>
2019-06-25 16:45:14 +0800

    add record to other data stores.

---
+++ cmd/orbitdb/cmd/put.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+	"github.com/orbitdb/go-orbit-db/http"
+)
+
+var putCmd = &cobra.Command{
+	Use:   "put",
+	Short: "Adds a record to a document database.",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 2 {
+			fmt.Println("Usage: orbitdb put name data")
+			os.Exit(1)
+		}
+
+		data := args[1]
+
+		fmt.Println(http.BuildURL(httpHost, "/db/", args[0]))
+		fmt.Println(http.Post(http.BuildURL(httpHost, "/db/", args[0]), data))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(putCmd)
+}
