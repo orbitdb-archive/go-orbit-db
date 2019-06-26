681dc4cb48f11e13903f35766f67e1fdc977537a
haydenyoung <hayden@knowledgearc.com>
2019-06-27 02:56:45 +0800

    Output index information about all records in database.

---
+++ cmd/orbitdb/cmd/index.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+	"github.com/orbitdb/go-orbit-db/http"
+)
+
+// indexCmd represents the index command
+var indexCmd = &cobra.Command{
+	Use:   "index",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Usage: orbitdb index name")
+			os.Exit(1)
+		}
+
+		fmt.Println(http.Get(http.BuildURL(httpHost, "/db/", args[0], "/index"), ""))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(indexCmd)
+}
